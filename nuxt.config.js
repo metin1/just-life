245046527f8d48a82775b2a4427746e3242fb1f4
Metin1 <metin3232@yahoo.com>
2022-12-03 13:30:50 +0300

    feat: settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,13 +5,14 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'just-life-3',
+    titleTemplate: '%s | My Just Life App',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Mj Just Life ' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
